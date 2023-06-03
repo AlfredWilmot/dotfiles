50e8257a82036daad30f8e9d694a752671420be3
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2023-06-03 16:50:45 +0000

    telescope finds hidden files but ignores .git dir

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,10 +1,14 @@
 local builtin = require('telescope.builtin')
 -- "Project-files" fuzzy file search
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({
+        hidden = true,
+        file_ignore_patterns = {".git/"},
+    });
+end)
 -- Search git files only
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 -- "Project-search" grep expressions in workspace
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-
