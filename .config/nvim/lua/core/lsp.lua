caa21d213fd8119a9899d985c1e5f4fa3373ecf8
AlfredWilmot <alfred.ingi.wilmot@gmail.com>
2025-02-23 19:28:40 +0000

    wip: experimenting with lua-lsp config

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -55,7 +55,28 @@ require('lspconfig').bashls.setup {
   capabilities = capabilities
 }
 require('lspconfig').lua_ls.setup {
-  capabilities = capabilities
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'Lua 5.3',
+        path = {
+          '?.lua',
+          '?/init.lua',
+          vim.fn.expand'~/.luarocks/share/lua/5.3/?.lua',
+          vim.fn.expand'~/.luarocks/share/lua/5.3/?/init.lua',
+          '/usr/share/5.3/?.lua',
+          '/usr/share/lua/5.3/?/init.lua'
+        }
+      },
+      workspace = {
+        library = {
+          vim.fn.expand'~/.luarocks/share/lua/5.3',
+          '/usr/share/lua/5.3'
+        }
+      }
+    }
+  }
 }
 require('lspconfig').rust_analyzer.setup {
   capabilities = capabilities
