9f28bf027a25538ffbb522691e9558570ee481f0
AlfredWilmot <>
2025-05-06 08:24:43 +0100

    adding basic nvim dap-ui config

---
+++ .config/nvim/lua/core/dap.lua
@@ -0,0 +1,11 @@
+local dap = require('dap') dap.configurations.python = {
+  {
+    type = 'python';
+    request = 'launch';
+    name = "Launch file";
+    program = "${file}";
+    pythonPath = function()
+      return '/usr/bin/env python'
+    end;
+  },
+}
